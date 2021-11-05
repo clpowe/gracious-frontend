814eae2e634342616bef66b005b61e2348425d2d
clpowe <clpowe@gmail.com>
2021-11-05 15:19:41 -0400

    ssr true

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
-  mode: 'universal',
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  ssr: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Gracious Coutour',
