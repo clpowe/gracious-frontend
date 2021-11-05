7f4ccb150481345ad64724c0d22a56d82d6daa83
clpowe <clpowe@gmail.com>
2021-11-05 10:47:04 -0400

    Update index.vue

--- pages/product/_id/index.vue
+++ pages/product/_id/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mx-auto mt-12 px-10 flex flex-col gap-5 m-10">
     <GoBack class="self-start justify-self-start" />
-    <div v-if="product" class="flex flex-col mx-auto">
+    <div v-if="product !== null" class="flex flex-col mx-auto">
       <client-only placeholder="Loading...">
         <v-zoomer
-          style="width: 100%; height: 500px; border: solid 1px silver"
+          style="width: 400px; height: 300px; border: solid 1px silver"
           class="flex-1 mx-auto px-10"
           :max-scale="5"
           @click="zoomIn((scale = 5))"
@@ -28,7 +28,7 @@
           class="snipcart-add-item btn w-36 mx-auto mt-10"
           :data-item-id="product.id"
           :data-item-price="product.price"
-          :data-item-url="`https://strapi-oji7.onrender.com/products/${product.id}`"
+          :data-item-url="`${url}${$route.fullPath}`"
           :data-item-description="product.description"
           :data-item-name="product.name"
           :data-item-max-quantity="1"
@@ -43,26 +43,32 @@
 
 <script>
 import 'vue-inner-image-zoom/lib/vue-inner-image-zoom.css'
-import productQuery from '../../../apollo/queries/product.gql'
+// import productQuery from '../../../apollo/queries/product.gql'
 
 export default {
   data() {
     return {
       product: null,
-      url: this.$route.fullPath,
+      url: process.env.storeUrl,
     }
   },
-  apollo: {
-    product: {
-      prefetch: true,
-      query: productQuery,
-      variables() {
-        return {
-          id: +this.$route.params.id,
-        }
-      },
-    },
+  async created() {
+    const res = await fetch(
+      `https://strapi-oji7.onrender.com/products/${this.$route.params.id}`
+    )
+    this.product = await res.json()
   },
+  // apollo: {
+  //   product: {
+  //     prefetch: true,
+  //     query: productQuery,
+  //     variables() {
+  //       return {
+  //         id: +this.$route.params.id,
+  //       }
+  //     },
+  //   },
+  // },
 }
 </script>
 
